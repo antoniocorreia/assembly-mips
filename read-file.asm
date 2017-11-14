4462d40404f9d34a6a6ab2ff5be246a4220c5145
antoniocorreia <antonio.alves.correia@gmail.com>
2017-11-14 04:16:15 -0300

    read all file

--- read-file.asm
+++ read-file.asm
@@ -1,5 +1,5 @@
 .data  
-fin: .asciiz "C:\Users\Antonio\Documents\GitHub\assembly-mips\file.txt"      # filename for input
+fin: .asciiz "file.txt"      # filename for input
 buffer: .space 1024
 message: .asciiz "Hello,"
 
@@ -18,25 +18,24 @@ openFile:
 	li   $a1, 0        # flag for reading
 	li   $a2, 0        # mode is ignored
 	syscall            # open a file 
-	move $s6, $v0      # save the file descriptor 
+	move $s0, $v0      # save the file descriptor 
 
 # reading from file just opened
 	
 	
 	
 	li   $v0, 14       # system call for reading from file
-	move $a0, $s6      # file descriptor 
+	move $a0, $s0      # file descriptor 
 	la   $a1, buffer   # address of buffer from which to read
 	li   $a2,  1024  # hardcoded buffer length
 	syscall            # read from file
 	
-	la $a0, buffer
 	li $v0, 4
+	la $a0, buffer
 	syscall
 
 # Close the file 
 
-	li   $v0, 16       # system call for close file
-	move $a0, $s6      # file descriptor to close
+	li   $v0, 10       # system call for close file
 	syscall            # close file
 	jr $ra
